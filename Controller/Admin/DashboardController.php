70b3bed798792e6b6d46109a71f7bf7edffb72bc
Martin de Keijzer <martin@spinninrecords.nl>
2020-07-29 15:30:45 +0200

    Add Easyadmin configuration

--- Controller/Admin/DashboardController.php
+++ Controller/Admin/DashboardController.php
@@ -5,6 +5,8 @@
  */
 namespace Martin1982\LiveBroadcastEasyadminBundle\Controller\Admin;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,5 +22,34 @@ class DashboardController extends AbstractDashboardController
     public function index(): Response
     {
         return parent::index();
+    }
+
+    /**
+     * Setup dashboard
+     *
+     * @return Dashboard
+     */
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            // the name visible to end users
+            ->setTitle('ACME Corp.')
+            // you can include HTML contents too (e.g. to link to an image)
+            ->setTitle('Live Broadcaster Admin')
+
+            // the domain used by default is 'messages'
+            ->setTranslationDomain('messages');
+    }
+
+    /**
+     * Setup menu
+     *
+     * @return iterable
+     */
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::linkToDashboard('Dashboard', 'fa fa-home'),
+        ];
     }
 }
