d1d3d3bb91a2d4f960005a8b8ba4e2c2f2aac1e1
Martin de Keijzer <martin@spinninrecords.nl>
2020-07-30 11:25:31 +0200

    Move config to php file

---
+++ Resources/config/admin.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This file is part of martin1982/live-broadcast-sonata-admin-bundle which is released under MIT.
+ * See https://opensource.org/licenses/MIT for full license details.
+ */
+namespace Martin1982\LiveBroadcastEasyadminBundle\DependencyInjection\Loader\Configurator;
+
+use Martin1982\LiveBroadcastEasyAdminBundle\Controller\Admin\AbstractChannelCrudController;
+use Martin1982\LiveBroadcastEasyadminBundle\Controller\Admin\DashboardController;
+use Martin1982\LiveBroadcastEasyadminBundle\Controller\Admin\LiveBroadcastCrudController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $container) {
+    $container->services()
+        ->set('livebroadcast.controller.dashboard', DashboardController::class)
+        ->public()
+        ->autowire()
+
+        ->set('livebroadcast.controller.channel_crud', AbstractChannelCrudController::class)
+        ->public()
+        ->autowire()
+
+        ->set('livebroadcast.controller.live_broadcast_crud', LiveBroadcastCrudController::class)
+        ->public()
+        ->autowire()
+    ;
+};
