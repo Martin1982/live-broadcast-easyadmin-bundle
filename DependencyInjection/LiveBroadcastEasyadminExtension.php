080876a92cbe34be2abf11cab739dafeab2b380e
Martin de Keijzer <martin@spinninrecords.nl>
2020-07-30 11:27:27 +0200

    Load php file in extension

--- DependencyInjection/LiveBroadcastEasyadminExtension.php
+++ DependencyInjection/LiveBroadcastEasyadminExtension.php
@@ -7,7 +7,7 @@ namespace Martin1982\LiveBroadcastEasyadminBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
@@ -22,7 +22,7 @@ class LiveBroadcastEasyadminExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('admin.php');
     }
 
