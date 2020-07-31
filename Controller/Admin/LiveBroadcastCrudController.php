f0b2bf5b770668bdfca1383373b09ea9a70241c0
Martin de Keijzer <martin@spinninrecords.nl>
2020-07-31 10:22:17 +0200

    Set choice field options

--- Controller/Admin/LiveBroadcastCrudController.php
+++ Controller/Admin/LiveBroadcastCrudController.php
@@ -46,7 +46,11 @@ class LiveBroadcastCrudController extends AbstractCrudController
             yield ImageField::new('thumbnail', 'Thumbnail (min. 1280x720px, 16:9 ratio)'),
             yield DateTimeField::new('startTimestamp', 'Broadcast start'),
             yield DateTimeField::new('endTimestamp', 'Broadcast end'),
-            yield ChoiceField::new('privacyStatus', 'Privacy status (YouTube only)'),
+            yield ChoiceField::new('privacyStatus', 'Privacy status (YouTube only)')->setChoices([
+                LiveBroadcast::PRIVACY_STATUS_PUBLIC => 'Public and listed',
+                LiveBroadcast::PRIVACY_STATUS_PRIVATE => 'Private',
+                LiveBroadcast::PRIVACY_STATUS_UNLISTED => 'Public and unlisted',
+            ]),
             yield BooleanField::new('stopOnEndTimestamp', 'Force the stream to stop on the end time')->setHelp('When checked video will be looped and ended when the end time is reached. When unchecked video will play once and ignore the end time, when the end time is later than the video length video may be restarted'),
             yield AssociationField::new('input', 'Video input'),
             yield AssociationField::new('outputChannels', 'Channels'),
