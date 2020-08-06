5abc9bfda6a305c212d07d33352fd5d82adf6ce8
Martin de Keijzer <martin@spinninrecords.nl>
2020-08-06 19:34:22 +0200

    Setup YouTube fields

--- Controller/Admin/YouTubeChannelCrudController.php
+++ Controller/Admin/YouTubeChannelCrudController.php
@@ -5,7 +5,9 @@
  */
 namespace Martin1982\LiveBroadcastEasyadminBundle\Controller\Admin;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Martin1982\LiveBroadcastBundle\Entity\Channel\ChannelYouTube;
 
 /**
@@ -21,5 +23,25 @@ class YouTubeChannelCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return ChannelYouTube::class;
+    }
+
+    /**
+     * Configure fields
+     *
+     * @param string $pageName
+     *
+     * @return iterable
+     */
+    public function configureFields(string $pageName): iterable
+    {
+        if (Action::NEW === $pageName || Action::EDIT === $pageName) {
+            return [
+                yield TextField::new('channelName'),
+                yield TextField::new('refreshToken'),
+                yield TextField::new('youTubeChannelName'),
+            ];
+        }
+
+        return [];
     }
 }
