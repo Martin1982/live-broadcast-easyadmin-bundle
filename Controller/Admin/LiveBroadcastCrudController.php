eb33c71c038a13fd86fccc414b16e7574e14a652
Martin de Keijzer <martin@spinninrecords.nl>
2020-08-06 18:54:24 +0200

    Configure CRUD items

--- Controller/Admin/LiveBroadcastCrudController.php
+++ Controller/Admin/LiveBroadcastCrudController.php
@@ -6,6 +6,8 @@
  */
 namespace Martin1982\LiveBroadcastEasyadminBundle\Controller\Admin;
 
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -32,6 +34,19 @@ class LiveBroadcastCrudController extends AbstractCrudController
     }
 
     /**
+     * Configure CRUD screen
+     *
+     * @param Crud $crud
+     *
+     * @return Crud
+     */
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setEntityLabelInSingular('Planned broadcast')
+            ->setEntityLabelInPlural('Planned broadcasts');
+    }
+
+    /**
      * Configure fields
      *
      * @param string $pageName
@@ -40,20 +55,31 @@ class LiveBroadcastCrudController extends AbstractCrudController
      */
     public function configureFields(string $pageName): iterable
     {
-        return [
-            yield TextField::new('name', 'Name'),
-            yield TextEditorField::new('description', 'Desccription'),
-            yield ImageField::new('thumbnail', 'Thumbnail (min. 1280x720px, 16:9 ratio)'),
-            yield DateTimeField::new('startTimestamp', 'Broadcast start'),
-            yield DateTimeField::new('endTimestamp', 'Broadcast end'),
-            yield ChoiceField::new('privacyStatus', 'Privacy status (YouTube only)')->setChoices([
-                'Public and listed'     => LiveBroadcast::PRIVACY_STATUS_PUBLIC ,
-                'Private'               => LiveBroadcast::PRIVACY_STATUS_PRIVATE,
-                'Public and unlisted'   => LiveBroadcast::PRIVACY_STATUS_UNLISTED,
-            ]),
-            yield BooleanField::new('stopOnEndTimestamp', 'Force the stream to stop on the end time')->setHelp('When checked video will be looped and ended when the end time is reached. When unchecked video will play once and ignore the end time, when the end time is later than the video length video may be restarted'),
-            yield AssociationField::new('input', 'Video input'),
-            yield AssociationField::new('outputChannels', 'Channels')->setFormTypeOption('multiple', true),
-        ];
+        if (Action::INDEX === $pageName) {
+            return [
+                'name',
+                'startTimestamp',
+            ];
+        }
+
+        if (Action::EDIT === $pageName || Action::NEW === $pageName) {
+            return [
+                yield TextField::new('name', 'Name'),
+                yield TextEditorField::new('description', 'Desccription'),
+                yield ImageField::new('thumbnail', 'Thumbnail (min. 1280x720px, 16:9 ratio)'),
+                yield DateTimeField::new('startTimestamp', 'Broadcast start'),
+                yield DateTimeField::new('endTimestamp', 'Broadcast end'),
+                yield ChoiceField::new('privacyStatus', 'Privacy status (YouTube only)')->setChoices([
+                    'Public and listed'     => LiveBroadcast::PRIVACY_STATUS_PUBLIC ,
+                    'Private'               => LiveBroadcast::PRIVACY_STATUS_PRIVATE,
+                    'Public and unlisted'   => LiveBroadcast::PRIVACY_STATUS_UNLISTED,
+                ]),
+                yield BooleanField::new('stopOnEndTimestamp', 'Force the stream to stop on the end time')->setHelp('When checked video will be looped and ended when the end time is reached. When unchecked video will play once and ignore the end time, when the end time is later than the video length video may be restarted'),
+                yield AssociationField::new('input', 'Video input'),
+                yield AssociationField::new('outputChannels', 'Channels')->setFormTypeOption('multiple', true),
+            ];
+        }
+
+        return parent::configureFields($pageName);
     }
 }
