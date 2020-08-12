5e025b191ca9b978b6c79886d853fb5600d1deed
Martin de Keijzer <martin@spinninrecords.nl>
2020-08-12 13:52:45 +0200

    Set default options

--- Form/Type/FacebookConnectType.php
+++ Form/Type/FacebookConnectType.php
@@ -8,6 +8,9 @@ namespace Martin1982\LiveBroadcastEasyadminBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class FacebookConnectType
@@ -26,5 +29,31 @@ class FacebookConnectType extends AbstractType
     {
         $builder->add('accessToken', TextType::class)
             ->add('fbEntityId', TextType::class);
+    }
+
+    /**
+     * Build form view
+     *
+     * @param FormView      $view
+     * @param FormInterface $form
+     * @param array         $options
+     *
+     * @return void
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['facebookAppId'] = $options['facebook_app_id'];
+    }
+
+    /**
+     * Configure form options
+     *
+     * @param OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('facebook_app_id', '');
     }
 }
--- Form/Type/YouTubeConnectType.php
+++ Form/Type/YouTubeConnectType.php
@@ -8,6 +8,9 @@ namespace Martin1982\LiveBroadcastEasyadminBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * Class YouTubeConnectType
@@ -26,5 +29,35 @@ class YouTubeConnectType extends AbstractType
     {
         $builder->add('refreshToken', TextType::class)
             ->add('youTubeChannelName', TextType::class);
+    }
+
+    /**
+     * Build form view
+     *
+     * @param FormView      $view
+     * @param FormInterface $form
+     * @param array         $options
+     *
+     * @return void
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['youTubeChannelName'] = $options['you_tube_channel_name'];
+        $view->vars['authUrl'] = $options['auth_url'];
+        $view->vars['youTubeRefreshToken'] = $options['you_tube_refresh_token'];
+    }
+
+    /**
+     * Configure form options
+     *
+     * @param OptionsResolver $resolver
+     *
+     * @return void
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('you_tube_channel_name', '');
+        $resolver->setDefault('auth_url', '');
+        $resolver->setDefault('you_tube_refresh_token', '');
     }
 }
