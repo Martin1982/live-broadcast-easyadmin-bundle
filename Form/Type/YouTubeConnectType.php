4f74fd473cee8e13dccf91fc3329650ed25686d6
Martin de Keijzer <martin@spinninrecords.nl>
2020-08-14 18:12:40 +0200

    Add youtube template variables

--- Form/Type/YouTubeConnectType.php
+++ Form/Type/YouTubeConnectType.php
@@ -5,16 +5,42 @@
  */
 namespace Martin1982\LiveBroadcastEasyadminBundle\Form\Type;
 
+use Martin1982\LiveBroadcastBundle\Exception\LiveBroadcastException;
+use Martin1982\LiveBroadcastBundle\Service\ChannelApi\Client\GoogleClient;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Class YouTubeConnectType
  */
 class YouTubeConnectType extends TextType
 {
+    /**
+     * @var GoogleClient
+     */
+    protected $googleClient;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
+    /**
+     * YouTubeConnectType constructor.
+     *
+     * @param GoogleClient $googleClient
+     * @param RequestStack $requestStack
+     */
+    public function __construct(GoogleClient $googleClient, RequestStack $requestStack)
+    {
+        $this->googleClient = $googleClient;
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * Build form view
      *
@@ -26,25 +52,42 @@ class YouTubeConnectType extends TextType
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        parent::buildView($view, $form, $options);
-        $view->vars['youTubeChannelName'] = $options['you_tube_channel_name'];
-        $view->vars['authUrl'] = $options['auth_url'];
-        $view->vars['youTubeRefreshToken'] = $options['you_tube_refresh_token'];
-    }
+        $client = $this->googleClient->getClient();
+        if (!$client instanceof \Google_Client) {
+            throw new LiveBroadcastException('Could not load the google client');
+        }
 
-    /**
-     * Configure form options
-     *
-     * @param OptionsResolver $resolver
-     *
-     * @return void
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        parent::configureOptions($resolver);
-        $resolver->setDefault('you_tube_channel_name', '');
-        $resolver->setDefault('auth_url', '');
-        $resolver->setDefault('you_tube_refresh_token', '');
+        $request = $this->requestStack->getCurrentRequest();
+        if (!$request) {
+            $request = new Request();
+        }
+
+        $session = $request->getSession();
+        if (!$session) {
+            $request->setSession(new Session());
+        }
+
+        $refreshToken = $session->get('youTubeRefreshToken');
+        if ($refreshToken) {
+            $client->fetchAccessTokenWithRefreshToken($refreshToken);
+        }
+
+        $accessToken = $client->getAccessToken();
+        $isAuthenticated = (bool) $accessToken;
+        $state = mt_rand();
+
+        if (!$isAuthenticated) {
+            $session->set('state', $state);
+            $session->set('authreferer', $request->getRequestUri());
+        }
+
+        $client->setState($state);
+
+        parent::buildView($view, $form, $options);
+        $view->vars['isAuthenticated'] = $isAuthenticated;
+        $view->vars['authUrl'] = $isAuthenticated ? '#' : $client->createAuthUrl();
+        $view->vars['youTubeChannelName'] = $session->get('youTubeChannelName');
+        $view->vars['youTubeRefreshToken'] = $session->get('youTubeRefreshToken');
     }
 
     /**
